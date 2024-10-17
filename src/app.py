f321b17992bce20c43a929d2a830533da87d184b
Gisela94 <gisela.garrido@hotmail.com>
2024-10-17 10:11:00 +0000

    Initial commit

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask import Flask, jsonify
-from flask import request
+from flask import Flask, jsonify, request
 
 todos = [
     { "label": "My first task", "done": False },
